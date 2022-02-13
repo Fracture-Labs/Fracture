401f4c2e053fc46b5bbc620c1c11b451e8c8a5d8
Niklas <niklas@long.ch>
2022-02-13 15:50:53 -0700

    feat: run cli in `main`

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use clap::Parser;
+use umbral_ipfs::{
+    cli::{Cli, Commands::*},
+    commands::*,
+};
+
+fn main() {
+    match Cli::parse().command {
+        Account => new_account(),
+        Encrypt(encrypt_args) => encrypt(encrypt_args),
+        Grant(grant_args) => grant(grant_args),
+        Pre(pre_args) => pre(pre_args),
+        Decrypt(decrypt_args) => decrypt(decrypt_args),
+    }
+}
