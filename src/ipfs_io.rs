4a4867f0a9782cc4fac122eff7580b28bb14792a
Mark Henderson <mark@mrh.io>
2022-02-12 18:57:51 -0700

    feat: add basic IPFS I/O

---
+++ src/ipfs_io.rs
@@ -0,0 +1,41 @@
+use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
+use std::io::Cursor;
+use futures::TryStreamExt;
+
+type CIDv0 = String;
+
+async fn write(bytes: Vec<u8>) -> CIDv0 {
+    let client = IpfsClient::default();
+    let data = Cursor::new(bytes);
+
+    match client.add(data).await {
+        Ok(res) => res.hash,
+        Err(e) => panic!("error adding file: {}", e)
+    }
+}
+
+async fn read(cid: CIDv0) -> Vec<u8> {
+    let client = IpfsClient::default();
+   
+    match client.cat(&cid).map_ok(|chunk| chunk.to_vec()).try_concat().await {
+        Ok(res) => res,
+        Err(e) => panic!("error getting CID: {}", e)
+    }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[tokio::test]
+  async fn writes_to_ipfs() {
+      let res = write("hello world".as_bytes().to_vec()).await;
+      assert_eq!("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD", res)
+  }
+
+  #[tokio::test]
+  async fn reads_from_ipfs() {
+      let content = read("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD".to_string()).await;
+      assert_eq!("hello world", String::from_utf8(content).as_ref().unwrap());
+  }
+}
