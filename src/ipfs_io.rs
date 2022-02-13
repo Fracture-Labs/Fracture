c44af1d015a17fb4acc49dca2a12010328d76bac
Niklas <niklas@long.ch>
2022-02-13 15:49:46 -0700

    chore: run nightly formatter

--- src/ipfs_io.rs
+++ src/ipfs_io.rs
@@ -1,6 +1,7 @@
-use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
 use std::io::Cursor;
+
 use futures::TryStreamExt;
+use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
 
 type CIDv0 = String;
 
@@ -10,32 +11,37 @@ async fn write(bytes: Vec<u8>) -> CIDv0 {
 
     match client.add(data).await {
         Ok(res) => res.hash,
-        Err(e) => panic!("error adding file: {}", e)
+        Err(e) => panic!("error adding file: {}", e),
     }
 }
 
 async fn read(cid: CIDv0) -> Vec<u8> {
     let client = IpfsClient::default();
-   
-    match client.cat(&cid).map_ok(|chunk| chunk.to_vec()).try_concat().await {
+
+    match client
+        .cat(&cid)
+        .map_ok(|chunk| chunk.to_vec())
+        .try_concat()
+        .await
+    {
         Ok(res) => res,
-        Err(e) => panic!("error getting CID: {}", e)
+        Err(e) => panic!("error getting CID: {}", e),
     }
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
+    use super::*;
 
-  #[tokio::test]
-  async fn writes_to_ipfs() {
-      let res = write("hello world".as_bytes().to_vec()).await;
-      assert_eq!("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD", res)
-  }
+    #[tokio::test]
+    async fn writes_to_ipfs() {
+        let res = write("hello world".as_bytes().to_vec()).await;
+        assert_eq!("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD", res)
+    }
 
-  #[tokio::test]
-  async fn reads_from_ipfs() {
-      let content = read("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD".to_string()).await;
-      assert_eq!("hello world", String::from_utf8(content).as_ref().unwrap());
-  }
+    #[tokio::test]
+    async fn reads_from_ipfs() {
+        let content = read("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD".to_string()).await;
+        assert_eq!("hello world", String::from_utf8(content).as_ref().unwrap());
+    }
 }
