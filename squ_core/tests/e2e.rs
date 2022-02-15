5886eadb95130f08f4235603e63215230bbb6075
Niklas <niklas@long.ch>
2022-02-15 16:35:12 -0700

    test: full e2e

--- squ_core/tests/e2e.rs
+++ squ_core/tests/e2e.rs
@@ -56,6 +56,7 @@ async fn e2e() {
         k_verifying_pk: None,
         d_pk: None,
         t_pks: None,
+        b_pk: None,
     };
     // ...and sends the public key to Freddie.
     freddie.s_pk = Some(fracture_aas.kp.1);
@@ -73,10 +74,10 @@ async fn e2e() {
 
     // The CIDs get sent to the trustees.
     trustee0.capsule_cid = Some(d_capsule_cid.clone());
-    trustee1.capsule_cid = Some(d_capsule_cid);
+    trustee1.capsule_cid = Some(d_capsule_cid.clone());
 
     trustee0.ciphertext_cid = Some(d_ciphertext_cid.clone());
-    trustee1.ciphertext_cid = Some(d_ciphertext_cid);
+    trustee1.ciphertext_cid = Some(d_ciphertext_cid.clone());
 
     trustee0.k_pk = Some(freddie.k_kp.clone().unwrap().1);
     trustee1.k_pk = Some(freddie.k_kp.clone().unwrap().1);
@@ -140,6 +141,7 @@ async fn e2e() {
 
     // Bob shares his pk with Freddie, freddie requests that his trustees generate the cfrags to
     // decrypt the d_sk.
+    let (b_sk, b_pk) = commands::new_account();
 
     // The trustees notify the Fracture DAO to update the state of the FractureAAS to allow it to generate the
     // kfrags for the data based on bob's pk.
@@ -155,7 +157,9 @@ async fn e2e() {
 
     let verified_cfrag = commands::pre(inner_pre_args);
 
-    // The trustees send the cfrags to the FractureAAS and it decrypts the d_sk.
+    // The trustees send the cfrags to the FractureAAS and it decrypts the d_sk. They also send
+    // bob's pk.
+    fracture_aas.b_pk = Some(b_pk);
     let cfrag = CapsuleFrag::from_array(&verified_cfrag.to_array()).unwrap();
 
     let inner_decrypt_args = commands::InnerDecryptArgs {
@@ -163,8 +167,8 @@ async fn e2e() {
         ciphertext: fracture_aas.k_ciphertext.unwrap(),
         cfrags: vec![cfrag],
         sender_pk: fracture_aas.k_pk.unwrap(),
-        receiver_sk: fracture_aas.kp.0,
-        receiver_pk: fracture_aas.kp.1,
+        receiver_sk: fracture_aas.kp.clone().0,
+        receiver_pk: fracture_aas.kp.clone().1,
         verifying_pk: fracture_aas.k_verifying_pk.unwrap(),
     };
 
@@ -172,11 +176,55 @@ async fn e2e() {
     let d_sk_bytes = commands::decrypt(inner_decrypt_args);
     let d_sk = SecretKey::from_bytes(d_sk_bytes).unwrap();
 
-    // Quick sanity check.
-    let d_capsule = Capsule::from_bytes(d_capsule).unwrap();
-    let plaintext = decrypt_original(&d_sk, &d_capsule, &d_ciphertext).unwrap();
-    assert_eq!(*plaintext, *PLAINTEXT.as_bytes());
+    // FractureAAS generates the kfrags for the data using the secret key and nukes the key.
+    let grant_args = GrantArgs {
+        sender_sk: d_sk,
+        receiver_pk: fracture_aas.b_pk.unwrap(),
+        threshold: D_THRESHOLD,
+        shares: D_SHARES,
+    };
 
+    let (d_verifying_pk, d_verified_kfrags) = commands::grant(grant_args);
+
+    // Send the kfrags to the proxies.
+    let d_kfrag = KeyFrag::from_array(&d_verified_kfrags[0].to_array()).unwrap();
+
+    // The proxies generate the cfrags to decrypt the plaintext.
+    // Q: how do they get the capsule CID, the server can't be trusted with that data.
+    let pre_args = PreArgs {
+        capsule_cid: d_capsule_cid.clone(),
+        kfrag: d_kfrag,
+        sender_pk: fracture_aas.d_pk.clone().unwrap(),
+        receiver_pk: b_pk,
+        verifying_pk: d_verifying_pk,
+    };
+
+    let inner_pre_args = commands::InnerPreArgs::from_pre_args(pre_args).await;
+    let d_verified_cfrag = commands::pre(inner_pre_args);
+
+    // Bob decrypts the data from the cfrags.
+    let d_cfrag = CapsuleFrag::from_array(&d_verified_cfrag.to_array()).unwrap();
+
+    // Q: where does bob get freddie's public key from?
+    let d_pk = fracture_aas.d_pk.unwrap();
+    // Q: where does bob get the ciphertext CID from?
+    // These issues go away if freddie's web of trust also decrypts the data. Otherwise the server
+    // could encrypt false data with freddie's secret key, the only mitigation is the capsule
+    // checks done by the proxies because they get the CID from freddie directly.
+    let decrypt_args = DecryptArgs {
+        capsule_cid: d_capsule_cid,
+        ciphertext_cid: d_ciphertext_cid,
+        cfrags: vec![d_cfrag],
+        sender_pk: d_pk,
+        receiver_sk: b_sk,
+        receiver_pk: b_pk,
+        verifying_pk: d_verifying_pk,
+    };
+
+    let inner_decrypt_args = commands::InnerDecryptArgs::from_decrypt_args(decrypt_args).await;
+    let plaintext = commands::decrypt(inner_decrypt_args);
+
+    assert_eq!(*plaintext, *PLAINTEXT.as_bytes());
 }
 
 struct Freddie {
@@ -206,4 +254,5 @@ struct FractureAAS {
     k_pk: Option<PublicKey>,
     k_verifying_pk: Option<PublicKey>,
     t_pks: Option<Vec<PublicKey>>,
+    b_pk: Option<PublicKey>,
 }
